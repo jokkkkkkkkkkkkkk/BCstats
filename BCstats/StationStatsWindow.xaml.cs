d1ef8691992dff8cf023e6769b076afe6e0460e5
jokkkkkkkkkkkkkk <20024jok@163.com>
2021-02-21 06:17:13 +0800

    添加新窗口：台站当月播出统计

---
+++ BCstats/StationStatsWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+
+namespace BCstats {
+    /// <summary>
+    /// StationStatsWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class StationStatsWindow : Window {
+
+        public StationStatsWindow(Window owner, DataTable dt) {
+            this.Owner = owner;
+            InitializeComponent();
+
+
+            StationStats_dataGridStats.ItemsSource = dt.DefaultView;
+            StationStats_dataGridStats.CanUserDeleteRows = false;
+            StationStats_dataGridStats.CanUserAddRows = false;
+            StationStats_dataGridStats.CanUserSortColumns = false;
+        }
+
+        private void StationStatsWindow_Loaded(object sender, RoutedEventArgs e) {
+
+
+        }
+
+        private void btn_ss_quit_Click(object sender, RoutedEventArgs e) {
+            this.ssWindow.Close();
+        }
+    }
+
+}
